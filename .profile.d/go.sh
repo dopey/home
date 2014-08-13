dcefb379e45e32970ca93e39bb71121dbdd04743
max furman <mx.furman@gmail.com>
2014-08-13 11:53:10 -0700

    update profile and some other stuff

--- .profile.d/go.sh
+++ .profile.d/go.sh
@@ -1,3 +1,1 @@
-#!/bin/sh
 export GOPATH="$HOME"
-export GOROOT="/usr/local/Cellar/go/1.2/libexec"
--- .profile.d/profile.sh
+++ .profile.d/profile.sh
@@ -60,6 +60,16 @@ export WORK
 export PYTHONPATH
 
 
+export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
+export HISTSIZE=100000                   # big big history
+export HISTFILESIZE=100000               # big big history
+shopt -s histappend                      # append to history, don't overwrite it
+
+# Save and reload the history after each command finishes
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+
+# Fixes Java stuff launching into the dock and messing with Spaces
+export _JAVA_OPTIONS=-Djava.awt.headless=true
 
 function vbp {
    nohup VBoxHeadless -startvm "betable-ubuntu64" &
