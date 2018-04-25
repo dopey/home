729d833828652fc87ab3b6b7aafc3ca3a76eb152
max furman <mx.furman@gmail.com>
2018-04-24 21:10:50 -0700

    aliases for git tagging

--- .profile.d/git.sh
+++ .profile.d/git.sh
@@ -16,8 +16,10 @@ alias gm='git merge'
 alias gp='git push'
 alias gpf='git push --force'
 alias gpm='git push origin master'
+alias gpt='git push origin tag'
 alias gpu='git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)'
 alias gs='git status'
+alias gt='git tag'
 
 function gr {
     if [ "$#" -ne 1 ]; then
