b9eba94119a5cbad699feeca6bfff3aae9d7a5ef
max furman <mx.furman@gmail.com>
2018-04-29 00:53:44 -0700

    go cov function for opening html window w/ gocov results

--- .profile.d/go.sh
+++ .profile.d/go.sh
@@ -7,3 +7,8 @@ if [[ "${OSTYPE}" == "linux-gnu" ]]; then
     # go
     PATH="${PATH}:/usr/local/go/bin:${GOPATH}/bin"
 fi
+
+cover () {
+    t="/tmp/go-cover.$$.tmp"
+    go test -coverprofile=$t $@ && go tool cover -html=$t && unlink $t
+}
